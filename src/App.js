84718dd43c218d80b89cb5cedb431903c1d8edf5
gonzalo <gonzalo.perez@viamericas.com>
2021-02-03 21:27:05 -0500

    mi_formulario-react-0-100

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [nombre, setNombre] = useState("Chalo Code!!");
+
+  const cambiarNombre = (e) => {
+    const value = e.target.value;
+    console.log(value);
+    setNombre(value);
+  };
+
+  const guardarClick = () => {
+    console.log("Esta es mi estdo local: ", nombre);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />       
       </header>
+      <h1>Mi formulario</h1>
+      <label>Nombre: </label>
+      <input
+        id="nombre"
+        name="nombre"
+        value={nombre}
+        onChange={cambiarNombre}
+      ></input>
+      <button onClick={guardarClick}>Guardar</button>
     </div>
   );
 }
