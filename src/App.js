39b8580a89c3e068a75e6ce70915803df063f6a2
gonzalo <gonzalo.perez@viamericas.com>
2021-03-14 20:40:22 -0500

    CSS TO SASS

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./AppSass.scss";
 
 function App() {
   const [nombre, setNombre] = useState("Chalo Code!!");
