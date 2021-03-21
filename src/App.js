3f383b475cabef51a80306ed47cd53f5062e79ee
gonzalo <gonzalo.perez@viamericas.com>
2021-03-20 19:53:23 -0500

    validaciones en formulario

--- src/App.js
+++ src/App.js
@@ -3,11 +3,32 @@ import logo from "./logo.svg";
 import "./AppSass.scss";
 
 function App() {
-  const [nombre, setNombre] = useState("Chalo Code!!");
+  const [nombre, setNombre] = useState("");
+  const [numberError, setNumberError] = useState(0);
 
   const cambiarNombre = (e) => {
     const value = e.target.value;
     console.log(value);
+    const minValue = value.length > 3;
+    const maxValue = value.length < 10;
+    const onliLet = /^[a-zA-Z\s]*$/g.test(value);
+
+    console.log("min", minValue);
+    console.log("maxValue", maxValue);
+    console.log("onliLet", onliLet);
+
+    if (onliLet == false) {
+      setNumberError(1);
+    } else if (!minValue) {
+      setNumberError(2);
+    } else {
+      setNumberError(3);
+    }
+
+    if (onliLet == true && minValue && maxValue) {
+      setNumberError(0);
+    }
+
     setNombre(value);
   };
 
@@ -33,8 +54,25 @@ function App() {
               onChange={cambiarNombre}
               className="mi_formulario__input"
             ></input>
+            {numberError == 1 && (
+              <label className="mi_formulario__error">
+                El nombre solo se puede incluir letras
+              </label>
+            )}
+
+            {numberError == 2 && (
+              <label className="mi_formulario__error">
+                El nombre minimo es 3 caracteres
+              </label>
+            )}
+
+            {numberError == 3 && (
+              <label className="mi_formulario__error">
+                El nombre maximo es 10 caracteres
+              </label>
+            )}
           </div>
-          <button onClick={guardarClick} className="mi_formulario__button">
+          <button disabled={numberError>0} onClick={guardarClick} className="mi_formulario__button">
             Guardar
           </button>
         </div>
